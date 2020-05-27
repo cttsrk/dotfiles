dea5ced77b063efaf1e3bf9b11948fac698b0f50
luser <luser@minion.local>
2020-05-27 10:47:40 +0200

    quickcommit 2020-05-27 10:47:40

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [ -f ~/.profile ]; then mv ~/.profile ~/profile.old; fi
-ln -s profile ~/.profile
+ln -s ~/.dotfiles/profile ~/.profile
 
 if [ -f ~/.tmux.conf ]; then mv ~/.tmux.conf ~/.tmux.conf.old; fi
-ln -s tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 if [ -f ~/.config/kitty/kitty.conf ]; then
     mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.old
 fi
-ln -s kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 
 if [ -f ~/.config/nvim/init.vim ]; then
     mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
 fi
-ln -s vimrc ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/vimrc ~/.config/nvim/init.vim
 
 rm ~/bin    # fails if bin is a directory
 ln -s ~/.dotfiles/bin ~/bin
