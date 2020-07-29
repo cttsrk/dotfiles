8aa6c3a27de8f2d4a02dc947000e1b5cc9b75491
luser <cttsrk@gmail.com>
2020-07-29 06:00:12 +0200

    quickcommit 2020-07-29 06:00:12

--- install.sh
+++ install.sh
@@ -8,17 +8,19 @@ ln -s ~/.dotfiles/profile ~/.profile
 if [ -f ~/.tmux.conf ]; then mv ~/.tmux.conf ~/.tmux.conf.old; fi
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
-
+if [ ! -d ~/.config/kitty ]; then mkdir -p ~/.config/kitty; fi 
 if [ -f ~/.config/kitty/kitty.conf ]; then
     mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.old
 fi
+
 if [ -f ~/.config/kitty/macos-launch-services-cmdline  ]; then
     mv ~/.config/kitty/macos-launch-services-cmdline  ~/.config/kitty/macos-launch-services-cmdline.old
 fi
+
 ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 ln -s ~/.dotfiles/kitty.macos ~/.config/kitty/macos-launch-services-cmdline
 
-
+if [ ! -d ~/.config/nvim ]; then mkdirp -p ~/.config/nvim; fi
 if [ -f ~/.config/nvim/init.vim ]; then
     mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.old
 fi
